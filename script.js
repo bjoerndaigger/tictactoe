b39901497105b0cdef74cffa952ce132a9a146ca
bjoerndaigger <bjoern.daigger@gmail.com>
2023-02-27 21:29:46 +0100

    onclick function

---
+++ script.js
@@ -0,0 +1,15 @@
+let fields = [];
+
+let currentShape = 'cross';
+
+function fillShape(id) {
+
+    if(currentShape == 'cross') {
+        currentShape = 'circle';
+    } else {
+        currentShape = 'cross';
+    }
+
+    fields[id] = currentShape;
+    console.log(fields);
+}
