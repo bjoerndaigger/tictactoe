03b8a754c9e21a4883deafd3de447eda71aa9194
bjoerndaigger <bjoern.daigger@gmail.com>
2023-03-01 17:00:10 +0100

    debug double click

--- script.js
+++ script.js
@@ -3,21 +3,23 @@ let fields = []; // Array, der die mit onclick übergebenen Werte speichert
 let currentShape = 'cross';  // Variable, die sagt, mit welchem Image gestartet werden soll
 
 function fillShape(id) { // Aufruf durch onclick und Übergabe von Parameter ID, welches Feld geklickt wurde
+    if (!fields[id]) { // Funktion wird wegen Negation Operator nur ausgeführt, wenn Bedingung undefined zurückgibt, also noch kein Wert vorhanden ist
+                       // so werden Doppelklicks verhindert.
+        if (currentShape == 'cross') { // if-Abfrage, die abwechselnd 'cross' und 'circle' in den Array gibt
+            currentShape = 'circle';
+            document.getElementById('player-1').classList.remove('player-inactive'); // entfernt Transparenz von Player 1 und
+            document.getElementById('player-2').classList.add('player-inactive'); // fügt Transparenz zu Player 2 hinzu
+        } else {
+            currentShape = 'cross';
+            document.getElementById('player-1').classList.add('player-inactive'); // fügt Transparenz zu Player 1 hinzu und
+            document.getElementById('player-2').classList.remove('player-inactive'); // entfernt Transparenz von Player 2
+        }
 
-    if (currentShape == 'cross') { // if-Abfrage, die abwechselnd 'cross' und 'circle' in den Array gibt
-        currentShape = 'circle';
-        document.getElementById('player-1').classList.remove('player-inactive'); // entfernt Transparenz von Player 1 und
-        document.getElementById('player-2').classList.add('player-inactive'); // fügt Transparenz zu Player 2 hinzu
-    } else {
-        currentShape = 'cross';
-        document.getElementById('player-1').classList.add('player-inactive'); // fügt Transparenz zu Player 1 hinzu und
-        document.getElementById('player-2').classList.remove('player-inactive'); // entfernt Transparenz von Player 2
+        fields[id] = currentShape; // pusht aktuelle Indexstelle (currentShape) mit 'cross' oder 'circle' in den Array
+        console.log(fields);
+        draw();
+        checkForWin();
     }
-
-    fields[id] = currentShape; // pusht aktuelle Indexstelle (currentShape) mit 'cross' oder 'circle' in den Array
-    console.log(fields);
-    draw();
-    checkForWin();
 }
 
 function draw() {  //entfernen von entsprechendem display-none im CSS beim click
@@ -32,10 +34,10 @@ function draw() {  //entfernen von entsprechendem display-none im CSS beim click
     }
 }
 
-function checkForWin() { 
+function checkForWin() {
     let winner; // Variable mit der ich überprüfe ob ein Wert vorhanden oder undefined ist
 
-    if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {  
+    if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
         // erfragt die verschiedenen Möglichkeiten, ob drei gleiche Werte in einer senk-, waagerechten oder diagonalen sind
         // außerdem ob Wert vorhanden (zweites &&), sonst wird nächste Zeile nicht ausgeführt
         winner = fields[0]; // überprüft ob Wert an Index 0, 1 oder 2 vorhanden, ansonsten undefined
