94c97cba865d7cd039609fcc19b1244cc1e4b2f5
bjoerndaigger <bjoern.daigger@gmail.com>
2023-03-01 10:23:42 +0100

    check winner

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-let fields = [];
+let fields = []; // Array, der die mit onclick übergebenen Werte speichert
 
-let currentShape = 'cross';
+let currentShape = 'cross';  // Variable, die sagt, mit welchem Image gestartet werden soll
 
-function fillShape(id) {
+function fillShape(id) { // Aufruf durch onclick und Übergabe von Parameter ID, welches Feld geklickt wurde
 
-    if (currentShape == 'cross') {
+    if (currentShape == 'cross') { // if-Abfrage, die abwechselnd 'cross' und 'circle' in den Array gibt
         currentShape = 'circle';
     } else {
         currentShape = 'cross';
     }
 
-    fields[id] = currentShape;
+    fields[id] = currentShape; // pusht aktuelle Indexstelle (currentShape) mit 'cross' oder 'circle' in den Array
     console.log(fields);
     draw();
+    checkForWin();
 }
 
-function draw() {
+function draw() {  //entfernen von entsprechendem display-none im CSS beim click
     for (let i = 0; i < fields.length; i++) {
         if (fields[i] == 'circle') {
             document.getElementById('circle-' + i).classList.remove('d-none');
@@ -24,5 +25,45 @@ function draw() {
         if (fields[i] == 'cross') {
             document.getElementById('cross-' + i).classList.remove('d-none');
         }
+    }
+}
+
+function checkForWin() {
+    let winner; 
+
+    if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) { 
+        winner = fields[0];
+    }
+
+    if (fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
+        winner = fields[3];
+    }
+
+    if (fields[6] == fields[7] && fields[7] == fields[8] && fields[7]) {
+        winner = fields[6];
+    }
+
+    if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
+        winner = fields[0];
+    }
+
+    if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
+        winner = fields[1];
+    }
+
+    if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
+        winner = fields[2];
+    }
+
+    if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
+        winner = fields[0];
+    }
+
+    if (fields[2] == fields[4] && fields[4] == fields[6] && fields[6]) {
+        winner = fields[2];
+    }
+
+    if (winner) {
+        console.log('GEWONNEN', winner);
     }
 }
